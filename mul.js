a42191551a2fc2d4eeb901a261a9fff92740a9a8
Berkay Berabi <berkay@deepcode.ai>
2020-07-23 11:31:17 +0200

    ..

--- mul.js
+++ mul.js
@@ -2,4 +2,4 @@ function myFunction(p1, p2) {
   return p1 * p2;   // The function returns the product of p1 and p2
 }
 
-var x = myFunction(11, 7);
+var y = myFunction(11, 10);
--- tocel.js
+++ tocel.js
@@ -1,5 +1,5 @@
 function toCelsius(fahrenheit) {
   return (5/9) * (fahrenheit-32);
 }
-document.getElementById("demo").innerHTML = toCelsius(77); 
+document.getElementById("demo").innerHTML = toCelsius(747); 
 document.getElementById("demo2").innerHTML = toCelsius(87);
